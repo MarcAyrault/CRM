0ff4ee2c407956924edbc0d799d119a278674b60
yangchen <1277687167@qq.com>
2024-10-17 21:43:58 +0800

    CRM初始提交

---
+++ src/com/bjpowernode/crm/settings/domain/User.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.crm.settings.domain;
+
+public class User {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
